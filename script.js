4075a7b7cdec4bdd88e1864545be80dfbf73da4b
hyper-pluto <90264586+hyper-pluto@users.noreply.github.com>
2023-02-27 18:59:14 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -8,64 +8,71 @@
 /* let myNumber = function() {
   console.log(mess.value); */
 //}
-let sNumber = Math.trunc(Math.random()*20) + 1;
+let sNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
-let theHs = document.querySelector('.highscore');
-let theScore = document.querySelector('.score');
-let mess1 = document.querySelector('.message');
-let numBox = document.querySelector('.numbers');
+let theHs = document.querySelector(".highscore");
+let theScore = document.querySelector(".score");
+let mess1 = document.querySelector(".message");
+let numBox = document.querySelector(".numbers");
 
-document.querySelector('.btn').addEventListener ('click', function() {
-  const guess = Number(document.querySelector('.guess').value);
+let btnFunction = function () {
+  const guess = Number(document.querySelector(".guess").value);
   //When there is no input
   if (!guess) {
-    mess1.textContent = '⛔ Not a number';
-  //when a player wins
+    mess1.textContent = "⛔ Not a number";
+    //when a player wins
   } else if (guess === sNumber) {
-     mess1.textContent = 'You guessed correct! 🥳';
-     numBox.style.backgroundColor = '#ff9000';
-     document.querySelector('.numbers').textContent = sNumber;
-     if (score >= highscore) {
-       highscore = score;
-       theHs.textContent = score;
-  }
-  //when a player guesses too high   
-} else if (guess > sNumber) {
-  if (score > 1) {
-    mess1.textContent = '📈 Too high';
-    score--;
-    theScore.textContent = score;
-} else {
-    mess1.textContent = 'You just lost the game. 😭';
-    theScore.textContent = 0;
- }
-}
-  //when a player guesses to low or loses
-   else if (guess < sNumber) {
-   if (score > 1) {
-     mess1.textContent = '📉 Too low';
-     score--;
-     theScore.textContent = score;
-   } else {
-      mess1.textContent = 'You just lost the game. 😭';
+    mess1.textContent = "You guessed correct! 🥳";
+    numBox.style.backgroundColor = "#ff9000";
+    document.querySelector(".numbers").textContent = sNumber;
+    if (score >= highscore) {
+      highscore = score;
+      theHs.textContent = score;
+    }
+    //when a player guesses too high
+  } else if (guess > sNumber) {
+    if (score > 1) {
+      mess1.textContent = "📈 Too high";
+      score--;
+      theScore.textContent = score;
+    } else {
+      mess1.textContent = "You just lost the game. 😭";
       theScore.textContent = 0;
-   }
-}
+    }
+  }
+  //when a player guesses to low or loses
+  else if (guess < sNumber) {
+    if (score > 1) {
+      mess1.textContent = "📉 Too low";
+      score--;
+      theScore.textContent = score;
+    } else {
+      mess1.textContent = "You just lost the game. 😭";
+      theScore.textContent = 0;
+    }
+  }
+};
+
+document.querySelector(".btn").addEventListener("click", function () {
+  btnFunction();
 });
 
-document.querySelector('.newbtn').addEventListener('click', function() {
+document.querySelector(".newbtn").addEventListener("click", function () {
   if (score >= theHs) {
     theHs.textContent = score;
   }
   score = 20;
-  sNumber = Math.trunc(Math.random()*20) + 1;
-  mess1.textContent = 'Start guessing...';
-  document.querySelector('.guess').value = '';
-  numBox.textContent = '?';
-  numBox.style.backgroundColor = '#9CFF00'
+  sNumber = Math.trunc(Math.random() * 20) + 1;
+  mess1.textContent = "Start guessing...";
+  document.querySelector(".guess").value = "";
+  numBox.textContent = "?";
+  numBox.style.backgroundColor = "#9CFF00";
 });
 
-
-
-
+document.addEventListener("keydown", function (e) {
+  console.log(e.key);
+  if (e.key === "Enter") {
+    btnFunction();
+  }
+});
